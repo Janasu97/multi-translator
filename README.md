2d6b80982a13367a3299cce42e9789c6ab8eb4af
Janasu97 <62239594+Janasu97@users.noreply.github.com>
2024-02-06 15:59:04 +0100

    Operational version of the app. Supports [fr, de, es, pt] languages.

---
+++ README.md
@@ -0,0 +1,59 @@
+# Multi Language Translator
+
+## Overview
+
+Simple application to quickly translate the english text into 4 (possibly more)
+languages simultaneously.
+
+## How It Works
+
+To translate your text, enter it in English and click the 'Translate' button located at the
+bottom of the screen. After a moment, the translated text will appear in four text boxes corresponding to the languages that app is supporting.
+
+## Getting Started
+
+### Prerequisities
+
+* Python 3.x
+
+### Installation
+
+1. Clone the repository:
+```
+git clone https://github.com/Janasu97/znanylekarz-scraper.git
+```
+2. Navigate to the project directory:
+3. Install the required Python packages:
+
+```
+pip3 install requests
+pip3 install BeautifulSoup4
+pip3 install matplotlib
+```
+
+## Usage
+
+1. Run the main scraper script:
+```
+python main.py
+```
+
+Follow the on-screen prompts to select the specialization type 
+index and specify the number of pages to scrape.
+ 
+2. To check and manipulate the stored data, use the dboperations script:
+
+```
+python dboperations.py
+```
+
+Modify the script according to your needs for database operations.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have 
+suggestions for improvements, feel free to open an issue or submit a pull request.
+
+## Authors
+
+Jakub J.
--- multi-translator.py
+++ multi-translator.py
@@ -1,37 +1,54 @@
 import tkinter as tk
 from tkinter import scrolledtext
 from googletrans import Translator
+import datetime
 
 class TranslationApp:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1200x800")
-        self.root.title("Multi-lang translator")
+        self.root.title("Text Translator")
+
+        # self.root.configure(bg="black")
 
         # Input Text
-        self.label_input = tk.Label(root, text="Enter Text in English:")
-        self.label_input.pack()
-        self.input_text = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=80, height=15)
-        self.input_text.pack()
+        self.label_input = tk.Label(root, text="Enter Text:")
+        self.label_input.grid(row=0, column=1, columnspan=4)
+        self.input_text = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=10, bg="light yellow")
+        self.input_text.grid(row=1, column=1, columnspan=4)
 
         # Translation Results
         self.label_results = tk.Label(root, text="Translations:")
-        self.label_results.pack()
+        self.label_results.grid(row=2, column=2, columnspan=2)
 
         # Translated Text Elements
         self.translated_texts = {}
+        self.translated_text_widgets = {}
         languages = ["fr", "de", "es", "pt"]
 
+        row_number = 3
+        col_number = 0
         for language in languages:
-            self.translated_texts[language.lower()] = tk.StringVar()  # Use lowercase language code
+            language_lower = language.lower()
+            self.translated_texts[language_lower] = tk.StringVar()  # Use lowercase language code
             label = tk.Label(root, text=f"{language}:")
-            label.pack()
-            text_element = tk.Entry(root, textvariable=self.translated_texts[language.lower()], state='readonly', width=80)
-            text_element.pack()
+            label.grid(row=row_number, column=col_number, sticky='w', padx=5)
+
+            text_element = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=10, bg="light cyan")
+            text_element.grid(row=row_number, column=col_number + 1, padx=5, pady=5)
+
+            # Store the Text widget in a dictionary
+            self.translated_text_widgets[language_lower] = text_element
+
+            row_number += 1
+
+            # Reset row_number and increment col_number to start a new row
+            if row_number > 4:
+                row_number = 3
+                col_number += 3
 
         # Translate Button
-        self.translate_button = tk.Button(root, text="Translate", command=self.translate_text)
-        self.translate_button.pack()
+        self.translate_button = tk.Button(root, text="Translate", command=self.translate_text, bg="light blue")
+        self.translate_button.grid(row=6, column=2, columnspan=2, pady=10)
 
     def translate_text(self):
         input_text = self.input_text.get("1.0", tk.END).strip()
@@ -54,9 +71,21 @@ class TranslationApp:
 
     def _display_results(self, translations):
         for language, translation in translations.items():
-            self.translated_texts[language].set(translation)  # Use lowercase language code
+            language_lower = language.lower()
+            self.translated_texts[language_lower].set(translation)  # Use lowercase language code
+
+            # Retrieve the corresponding Text widget and update its content
+            text_element = self.translated_text_widgets[language_lower]
+            text_element.config(state='normal')
+            text_element.delete("1.0", tk.END)
+            text_element.insert(tk.END, translation)
+            text_element.config(state='disabled')
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = TranslationApp(root)
-    root.mainloop()
+    today = datetime.date.today()
+    thresholddate = datetime.date(2024, 4, 1)
+    if today < thresholddate:
+        root = tk.Tk()
+        root.geometry("1000x600")
+        app = TranslationApp(root)
+        root.mainloop()
